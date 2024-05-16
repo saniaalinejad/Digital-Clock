9953007edca1b7ec506e4d9c4903a355595c4c89
Sania Alinejad <152985607+saniaalinejad@users.noreply.github.com>
2024-05-16 10:56:40 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//let $ = document
+//const hourElem = $.querySelector('#hour')
+//const minuteElem = $.querySelector('#minute')
+//const secondElem = $.querySelector('#seconds')
+ let $ = document
+ const hourElem = $.querySelector('#hour')
+ const minuteElem = $.querySelector('#minute')
+ const secondElem = $.querySelector('#seconde')
+
+//setInterval(function () {
+    //let myDate = new Date()
+    setInterval(function () {
+        let myDate = new Date()
+        let nowHour = myDate.getHours()
+        let nowMinute = myDate.getMinutes()
+        let nowSecond = myDate.getSeconds()
+
+        if (nowHour <10){
+            nowHour = '0' + nowHour
+        }
+        if (nowMinute <10) {
+            nowMinute = '0' + nowMinute
+        }
+        if (nowSecond <10){
+            nowSecond = '0' + nowSecond
+        }
+
+        hourElem.innerHTML = nowHour
+        minuteElem.innerHTML = nowMinute
+        secondElem.innerHTML = nowSecond
+
+        console.log(myDate);
+    },1000);
+
+    //let nowHour = myDate.getHours()
+    //let nowMinute = myDate.getMinutes()
+    //let nowSecond = myDate.getSeconds()
+
+    //if (nowHour < 10) {
+        //nowHour = '0' + nowHour
+    //}
+    //if (nowMinute < 10) {
+        //nowMinute = '0' + nowMinute
+    //}
+    //if (nowSecond < 10) {
+        //nowSecond = '0' + nowSecond
+    //}
+
+    //hourElem.innerHTML = nowHour
+    //minuteElem.innerHTML = nowMinute
+    //secondElem.innerHTML = nowSecond
+
+    //console.log(myDate);
+//}, 1000);
